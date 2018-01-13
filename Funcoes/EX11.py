07587e46415bcd794cba42e0aed0fc47ccc5cf5e
Victor Hugo <victorshgo@outlook.com>
2018-01-13 16:41:27 -0200

    Update EX11.py

--- Funcoes/EX11.py
+++ Funcoes/EX11.py
@@ -3,7 +3,11 @@ def data(d, m, ano):
     if((d<=0 or d>31) or (m<1 or m>12) or (ano<0)):
         print("Valor inválido.")
     else:
-        print(d, " de ", mes[m-1], " de ", ano)
+        if(m==2):
+            if(d>29):
+                print("Valor inválido.")
+            else:
+                print(d, " de ", mes[m-1], " de ", ano)
 
 d = int(input("Dia: "))
 m = int(input("Mês: "))
